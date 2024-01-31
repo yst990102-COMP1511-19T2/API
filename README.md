ea57402ff9d47bf9c728796b3112b1e2d996c1ad
Shi Tong Yuan <z5192519@ad.unsw.edu.au>
2024-01-31 16:09:23 +1100

    update
    1. 测试文件到acitivies文件夹
    2. 添加根目录sh和readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# API
+这里保存了所有lab和assign的自动化测试的文件，包括所有的测试用例和测试数据。
+
+源地址为：/web/cs1511/19T2/public/activities 
+现地址为：https://raw.githubusercontent.com/yst990102-COMP1511-19T2/API/main
+
+autotest.sh是对应的自动测试脚本，需要结合[COMP1511/autotest](https://github.com/COMP1511UNSW/autotest)进行使用，我在[这里](https://github.com/yst990102-COMP1511-19T2/autotest)做了相应的仓库备份。
---
+++ autotest.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# config.sh只适用于vlab机器上，根目录有备份，这里需要注释掉
+# config_file="$(dirname "$(readlink -f "$0")")"/config.sh
+
+# work_dir="$(dirname "${ASSIGNDIR}")"
+# work_dir_name="$(basename "${work_dir}")"
+# unsw_session="${work_dir_name%.work}"
+# course_account="$(basename "$(dirname "${work_dir}")")"
+
+# base_dir="/web/${course_account}/${unsw_session}"
+
+# # handling  being run by give from dryrun
+# test ! -r "$config_file" &&
+# 	config_file="./1511_config.sh"
+
+# test -r "$config_file" &&
+# 	. "$config_file"
+
+autotest="/home/yst990102/COMP1511/autotest/autotest.py"
+test -f "$autotest" || {
+	echo "Internal error can not find find autotest script"
+	exit 1
+}
+
+# if ASSIGNDIR is set, assume we are being run as give's dryrun script and create !dryrun_record
+if test -z "$ASSIGNDIR"
+then
+	python3 -I "$autotest" --exercise_directory "./activity" --parameters "$parameters" "$@"
+else
+	python3 -I -u "$autotest" --exercise_directory "./activity" --parameters "$parameters" "$@" |
+	tee '!dryrun_record'
+fi
